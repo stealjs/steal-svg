03774525a4750a64cbd97fe387d54547ab106618
Marshall Thompson <marshall@creativeideal.net>
2016-12-27 10:37:35 -0700

    Clean up

--- steal-svg.js
+++ steal-svg.js
@@ -5,7 +5,5 @@ exports.translate = function (load) {
   load.metadata.format = 'es6';
   load.source = load.source.replace(xmlTagRegex, '');
   load.source = load.source.replace(nsRegex, '');
-
-  // if we found a leading < we expect that we have proper html
   load.source = `export default \`${load.source}\`;`;
 };
