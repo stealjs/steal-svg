8cc684aa65b1005d3d7364d173b85ac15307a069
Mike 'mitch' Mitchel <mike@mikemitchel.com>
2017-07-21 09:45:44 -0500

    Update final export line for IE11
    IE11 had trouble with the string interpolation, and this change fixed it. In addition, I had to remove the xml/xmlns regular expressions to get the SVGs to load in IE11, as it kept throwing a regular expression syntax error. Removing the reg ex did not break this in any of the main modern browsers, but it might be just for my particular use case, so I did not add their removal to this PR.

--- steal-svg.js
+++ steal-svg.js
@@ -5,5 +5,5 @@ exports.translate = function (load) {
   load.metadata.format = 'es6';
   load.source = load.source.replace(xmlTagRegex, '');
   load.source = load.source.replace(nsRegex, '');
-  load.source = `export default \`${load.source}\`;`;
+  load.source = "export default `" + load.source + "`;";
 };
